df9ca6295e74c1c1798affdcc5c6c76d14a8de08
Egenie-mih <astra2826@gmail.com>
2018-01-25 12:07:46 +0300

    Change file structure,minimize js and fix pixel perfect

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  if (!Element.prototype.matches) {
-      Element.prototype.matches =
-          Element.prototype.matchesSelector ||
-          Element.prototype.mozMatchesSelector ||
-          Element.prototype.msMatchesSelector ||
-          Element.prototype.oMatchesSelector ||
-          Element.prototype.webkitMatchesSelector ||
-          function(s) {
-              var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-                  i = matches.length;
-              while (--i >= 0 && matches.item(i) !== this) {}
-              return i > -1;
-          };
-  }
-
   var itemClickHandler = function (e) {
-    if(!e.target.matches('.catfood__buy, .catfood__check')) {
-      var checkedItem = this.classList;
+      var checkedItem = this.parentNode.classList;
       var disabledItem = checkedItem.contains('catfood__item--disabled');
 
       if (!disabledItem) {
         checkedItem.toggle('catfood__item--checked');
         checkedItem.remove('catfood__item--checked-hover');
       }
-    }
   }
 
   var itemOverHandler = function (e) {
-    if (this.classList.contains('catfood__item--checked')) {
-      this.classList.add('catfood__item--checked-hover');
+    if (this.parentNode.classList.contains('catfood__item--checked')) {
+      this.parentNode.classList.add('catfood__item--checked-hover');
     }
   }
 
   var clickHandler = function (e) {
     e.preventDefault();
-    this.parentNode.parentNode.parentNode.querySelector('.catfood__item').classList.add('catfood__item--selected');
+    this.parentNode.parentNode.classList.add('catfood__item--checked');
   }
 
-  var items = document.querySelectorAll('.catfood__item');
+  var items = document.querySelectorAll('.catfood__item .catfood__card');
   var itemsBuy = document.querySelectorAll('.catfood__item .catfood__buy-button');
 
   Array.prototype.forEach.call(items, function (item) {
